f1416a3ae559fe4de316aef3e5ce21656516048d
Barry Tabios <b_tabios@yahoo.com>
2017-02-09 14:43:28 +1100

    thursday warmups and code alongs

---
+++ wk01/Thursday/the_board.js
@@ -0,0 +1,31 @@
+console.log('JS WORKING for The Board');
+
+var row_size = 8;
+var column_size = 8;
+var row = '';
+//var column = [];
+var grid_size = 64;
+//var board = [];
+var row_string = [];
+
+for (var j=0; i<column_size; j++ ){
+  for (var i=0; i<row_size; i++){
+   row += '# ';
+  // if(i%16 === 0 && i !== 0)
+   //{
+    //row += '\n ';
+   //};
+  };
+  console.log(row);
+  row += '\n';
+};
+
+
+
+
+
+
+
+
+//console.log(row);
+//onsole.log(row.length);
