29a670b23d686679e1436580d526f11b96f43b26
Barry Tabios <b_tabios@yahoo.com>
2017-02-09 20:26:31 +1100

    updated and completed board game

--- wk01/Thursday/the_board.js
+++ wk01/Thursday/the_board.js
@@ -1,6 +1,32 @@
 console.log('JS WORKING for The Board');
 
-var row_size = 8;
+var row = prompt('enter row size'); // prompts user to enter a value for row
+var input_column = prompt('enter column size'); // prompts user to enter a value for column
+
+var column = input_column * 2;  // multiplies column by 2
+
+var even_row = ""; // creates string for an even row
+var odd_row = ""; // creates variabl string for odd rw
+
+
+for (i = 0; i < row; i++) { // loops until it reaches number for rows
+
+  if (i % 2 === 0) { // checks if row is an even
+    while (even_row.length < column) { // if row is even do this
+      even_row += '# '; // creates string with 'space after #'
+    };
+    console.log(even_row); // prints string to console
+  } else { // else if do this
+    while (odd_row.length < column) { // performs loop if row is odd
+      odd_row += ' #' // creates string with 'space' before the #
+    };
+    console.log(odd_row); // prints string to console
+  };
+
+};
+
+
+/*var row_size = 8;
 var column_size = 8;
 var row = '';
 //var column = [];
@@ -18,7 +44,7 @@ for (var j=0; i<column_size; j++ ){
   };
   console.log(row);
   row += '\n';
-};
+};*/
 
 
 
